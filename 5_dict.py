5ed055b3938f6a8664ebbf5419bfe0c748d1b5b9
alexmudra <alexander.kobko@gmail.com>
2019-01-14 21:33:48 +0200

    added homework 4, start to add more countries in SET

--- 5_dict.py
+++ 5_dict.py
@@ -1,38 +1,17 @@
 
+phones = [
+        {'brand': 'samsung','model':'a7','display_width':1980, 'display_heith':1080,'is_gprs':'Yes', 'is_wi-fi':'Yes'},
 
-samsung = {'model':'a7',
-        'display_width':1980,
-        'display_heith':1080,
-        'is_gprs':'Yes',
-        'is_wi-fi':'Yes'
-        }
-iphone = {
-        'model':7,
-        'display_width':2200,
-        'display_heith':1080,
-        'is_gprs':'Yes',
-        'is_wi-fi':'Yes'
-         }
-xiaomy = {
-        'model':8,
-        'display_width':2200,
-        'display_heith':1080,
-        'is_gprs':'Yes',
-        'is_wi-fi':'Yes'
-        }
-nokia = {
-        'model':6.01,
-        'display_width':1800,
-        'display_heith':900,
-        'is_gprs':'Yes',
-        'is_wi-fi':'Yes'
-        }
-print(samsung.items())
+        {'brand': 'iphone','model':7,'display_width':2200, 'display_heith':1080,'is_gprs':'Yes' ,'is_wi-fi':'Yes'},
 
+        {'brand': 'xiaomy','model':8,'display_width':2200,'display_heith':1080,'is_gprs':'Yes','is_wi-fi':'Yes'},
 
+        {'brand': 'nokia','model':6.01,'display_width':1800,'display_heith':900,'is_gprs':'Yes','is_wi-fi':'Yes'}
 
-for i in samsung:
-    #if 'display_width' > 2000 and 'display_heith' >= 1000:
-        print(i['display_width'])
+]
+
+for i in phones:
+        if i['display_width'] > 1900 & i["display_heith"] >= 1000:
+                print("dslfklsdkf")
 
 
