465c7c5cc83e38ff8355b3e6ce062ac862cc211c
mudra <alexander.kobko@gmail.com>
2019-01-21 20:11:55 +0200

    added 3 def functions schengen_calculator

--- schengen_calculator_via_functions.py
+++ schengen_calculator_via_functions.py
@@ -1,5 +1,5 @@
-residence_limit = 90  # 45, 60
-schengen_constraint = 180
+RESIDENCE_LIMIT = 90  # 45, 60 (КОНСТАНТА)
+SCHENGEN_CONSTRAINT = 180 #(КОНСТАНТА)
 visits = [[1, 10], [61, 90], [101, 140], [141, 160], [271, 290]]
 
 
@@ -12,7 +12,7 @@ def calc_days_for_visits(visits):
     for visit in visits:
         days_for_visit = 0
         for past_visit in visits:
-            if visit[0] - schengen_constraint < past_visit[0] < visit[0]:
+            if visit[0] - SCHENGEN_CONSTRAINT < past_visit[0] < visit[0]:
                 days_for_visit += get_visit_length(past_visit)
         days_for_visit += get_visit_length(visit)
         days_for_visits.append(days_for_visit)
@@ -22,18 +22,18 @@ def calc_days_for_visits(visits):
 days_for_visits = calc_days_for_visits(visits)
 
 for visit, total_days in zip(visits, days_for_visits):
-    if total_days > residence_limit:
-        overstay_time = total_days - residence_limit
+    if total_days > RESIDENCE_LIMIT:
+        overstay_time = total_days - RESIDENCE_LIMIT #рахує перевищення ліміту в перебування країнаї ЄС
         print('Во время визита', visit, 'количество время пребывания превышено на', overstay_time, 'дней')
 
 
 def predict_visit_days(day_in_future):
-    visits_with_future = visits + [[day_in_future, day_in_future]]
+    visits_with_future = visits + [[day_in_future, day_in_future]] #підрахує візити у майбутньому
     days_for_visits_in_future = calc_days_for_visits(visits_with_future)
 
     days_in_es_for_future_visit = days_for_visits_in_future[len(days_for_visits_in_future) - 1]
 
-    days_we_can_stay_in_es = residence_limit - days_in_es_for_future_visit + 1
+    days_we_can_stay_in_es = RESIDENCE_LIMIT - days_in_es_for_future_visit + 1
     print('Я смогу пробыть в шенеге {0} дней, если въеду {1} числа'.format(days_we_can_stay_in_es, day_in_future))
 
 
