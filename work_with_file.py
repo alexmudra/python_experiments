9c6e78a962ea14f524f0c1df87d2b24b62e3eb39
alexmudra <alexander.kobko@gmail.com>
2019-02-03 21:14:05 +0200

    added file for avg temperature, write in file avg.temperature

--- work_with_file.py
+++ work_with_file.py
@@ -35,7 +35,7 @@ Info from txt file [-8, -2, -10, -5, 6, 12, -20]
 Average winter temper.in Kyiv: -3.8571
 '''
 
-#----------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # Приклад як ,через ф-ію .strip видалимо лишню табуляцію, перенос каретки, пробіл
 
 with open('temperature.txt') as doc:
@@ -46,4 +46,24 @@ with open('temperature.txt') as doc:
 print("Info from txt file and .strip", temperature)
 '''
 Info from txt file and .strip [-8, -2, -10, -5, 6, 12, -20, '-8', '-2', '-10', '-5', '6', '12', '-20']
+
+
 '''
+#----------------------------------------------------------Прочитаєм і запишем файл температуру
+temperature = []
+
+with open('temperature.txt') as doc:
+    for line in doc:
+        temperature.append(int(line))
+
+print("Info from txt file", temperature)
+avg_temperature = sum(temperature) / len(temperature)
+
+#використаємо ф-ію round щоб округлити флоат число
+print("Average winter temper.in Kyiv:", round(avg_temperature, 4))
+
+#створимо файл і запишем в файл середню температуру(із флагом w)
+with open('average_temp_file.txt', 'w') as avg:
+    avg.write(str(avg_temperature))
+
+
