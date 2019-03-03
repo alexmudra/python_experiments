017a64050258234251590a534d747a72d4c7631f
alexmudra <alexander.kobko@gmail.com>
2019-03-03 22:43:52 +0200

    added first code with selenium

--- selenium/simple_test.py
+++ selenium/simple_test.py
@@ -1,42 +1,29 @@
 from selenium import webdriver
-driver = webdriver.Firefox()
-driver.get('https://python-scripts.com/web-automation-with-python-and-selenium')
+from selenium.webdriver.common.keys import Keys
+
+dr= webdriver.Chrome()
+dr.get("https://habr.com/ru/post/29980/")
+print('This is site title:', dr.title)
 
 
-# from selenium.webdriver import Firefox
-# from selenium.webdriver.firefox.options import Options
-#
-# opts = Options()
-# opts.set_headless()
-# assert opts.headless
-#
-# browser = Firefox(options=opts)
-# browser.get('https://duckduckgo.com')
+dr.find_element_by_class_name('logo')
+print('Logo is available')
+
+dr.contextMenu(dr.find_element_by_class_name('logo'))
 
 
+dr.close()
+dr.quit()
 
-# Switch to new window
-# driver.switch_to_window(driver.window_handles[-1])
-# print(" Twitter window should go to facebook ")
-# print ("New window ", driver.title)
-# driver.get("http://facebook.com")
-# print ("New window ", driver.title)
-#
-# # Switch to old window
-# driver.switch_to_window(driver.window_handles[0])
-# print (" Linkedin should go to gmail ")
-# print ("Old window ", driver.title)
-# driver.get("http://gmail.com")
-# print ("Old window ", driver.title)
-#
-# # Again new window
+
+# Again new window
 # driver.switch_to_window(driver.window_handles[1])
 # print (" Facebook window should go to Google ")
 # print ("New window ", driver.title)
 # driver.get("http://google.com")
 # print ("New window ", driver.title)
 #
-# #
+#
 # # browser = webdriver.Firefox()
 # body = browser.find_element_by_tag_name('body')
 # body.send_keys(Keys.CONTROL + 'n')
