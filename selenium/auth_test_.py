700016e6f6e7e932ddf8e48fdbcf8e7c84a6cbea
mudra <alexander.kobko@gmail.com>
2019-03-04 20:11:34 +0200

    stack with using sendKeys

--- selenium/auth_test_.py
+++ selenium/auth_test_.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-dr= webdriver.Chrome()
-dr.get("http://www.dzo.byustudio.in.ua/")
+dr = webdriver.Chrome()
+dr.get("http://www.dzo.byustudio.in.ua/cabinet")
+fill_pass = dr.find_element_by_xpath("//div[@class='inp']//input[@name='email']")
+fill_pass.sendKeys('sdklfjkdjsf')
+
+
 print('This is site title:', dr.title)
-dr.implicitly_wait(30)
-
-dr.find_element_by_class_name('authBtn').click()
 
 
 
 
-dr.close()
-dr.quit()
+#dr.implicitly_wait(30)
+#dr.close()
+#dr.quit()
