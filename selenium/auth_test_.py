013c7e9ba8e9e24782418312c0db11c9a5b2512f
alexmudra <alexander.kobko@gmail.com>
2019-03-10 21:55:11 +0200

    added login to tender DZO

--- selenium/auth_test_.py
+++ selenium/auth_test_.py
@@ -3,17 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 dr = webdriver.Chrome()
+log = ("organizator_main@byustudio.in.ua")
+ps = "VNNcY7989"
+
 dr.get("http://www.dzo.byustudio.in.ua/cabinet")
-fill_pass = dr.find_element_by_xpath("//div[@class='inp']//input[@name='email']")
-fill_pass.sendKeys('sdklfjkdjsf')
+dr.find_element_by_xpath("//div[@class='inp']//input[@name='email']").send_keys(log)
+dr.implicitly_wait(30)
+dr.find_element_by_xpath("/html/body/div[2]/section/div/section[2]/div/section/form/section[1]/table/tbody/tr[2]/td[2]/div/input").send_keys(ps)
+dr.find_element_by_xpath("/html/body/div[2]/section/div/section[2]/div/section/form/section[2]/table/tbody/tr[1]/td[2]/div/div[1]/div/button").click()
+print("==========================================")
+print()
+print("Танюшка, дивись що ми знайшли: ))", dr.title)
+print()
+print("==========================================")
+#assert "Кабінет - Мої закупівлі" != dr.title
 
+dr.find_element_by_xpath("//a[contains(text(),'Поточні тендери')]").click()
+dr.find_element_by_xpath("//a[contains(text(),'Створити закупівлю')]").click()
+dr.find_element_by_xpath("//*[@id='tender']/section[1]/table/tbody/tr[5]/td[2]/div/div[1]/div[1]/input").send_keys(100)
 
-print('This is site title:', dr.title)
-
-
-
-
-#dr.implicitly_wait(30)
 #dr.close()
 #dr.quit()
