b9babd5e131d2e36414ab814c0a62de2b6a5066d
аlexmudra <alexander.kobko@gmail.com>
2019-03-26 22:48:47 +0200

    added def function sum numbers and return result

--- def_hello_w.py
+++ def_hello_w.py
@@ -27,3 +27,23 @@ def print_square1 (num):
 
 print_square1(3)
 #9
+#////////////////////////////////////////////
+
+# Напишите функцию add_numbers, которая возвращает результат сложения двух своих
+# параметров first и second.Дополнительные переменные не использовать.
+#
+# # ...
+# # code
+# # ...
+
+def add_numbers(first, second):
+
+    return first + second
+result = add_numbers(2,4)
+print(result)
+#6
+
+def print_square(num):
+    return num * num
+print(print_square(2))
+
