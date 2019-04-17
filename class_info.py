559834f2993b05a38e215145bde324000016903a
mudra <alexander.kobko@gmail.com>
2019-04-17 20:19:39 +0300

    start working with OOP (https://itvdn.com/ru/video/python-essential)

---
+++ class_info.py
@@ -0,0 +1,18 @@
+"""
+написать клас который не возвращает ничего
+"""
+
+class MyClass:
+    pass #пасс не возвращает ничего
+
+
+"""
+Напишите класс MyObject, который содержит поля int_field и str_field со значениями 5 и
+“simple string” соответственно. Создайте экземпляры класса MyObject, с названиями obj1 и obj2
+"""
+
+class MyObject:
+    int_field = 5 #атрибути класа
+    str_field = "simple string" #атрибути класа
+obj1 = MyObject() # створили екземпляри класа MyObject
+obj2 = MyObject()
