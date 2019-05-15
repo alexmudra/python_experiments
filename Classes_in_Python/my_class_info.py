ff237bde24e623bef522b7ac0cace7d98cc821c3
mudra <alexander.kobko@gmail.com>
2019-05-15 20:36:50 +0300

    added simple, private and protected attributes

--- Classes_in_Python/my_class_info.py
+++ Classes_in_Python/my_class_info.py
@@ -19,6 +19,10 @@ class MyClass:
 
 #////////////////////////////////////
 
+
+
+
+
 """
 Создайте экземпляры класса MyObject с именем obj1 и obj2. Присвойте полю int_field объекта obj1 значение 10,
 а полю str_field значение “string2”. Выведите в одной строке значение полей obj1, а во второй строке, значение полей obj2.
@@ -143,28 +147,28 @@ if __name__ == "__main__": # умова якщо наш метод був зап
 который будет возвращать равносторонний треугольник со стороной side, равной side_a четырехугольника Rectangle.
 """
 
-class Rectangle:
-
-    def __init__(self, side_a, side_b): # метод конструктор який приймає 2 параменти: 2 сторони
-        self.side_a = side_a # атрибут данних
-        self.side_b = side_b
-
-    def __repr__(self): # метод __repr__ повертає строкове значення об'єкта. Метод конвертує об'єкт в строку
-        # яке тільки для внутрішнього користування
-        return "Rectangle(%.1f, %.1f)" % (self.side_a, self.side_b)
-
-class Triangle:
-
-    def __init__(self, side):
-        self.side = side
-
-    def __repr__(self):
-        return "Triangle(%.f)" % self.side
-
-    @classmethod
-    def from_rectangle(cls, rectangle):
-        side = rectangle.side_a
-        return cls(side)
+# class Rectangle:
+#
+#     def __init__(self, side_a, side_b): # метод конструктор який приймає 2 параменти: 2 сторони
+#         self.side_a = side_a # атрибут данних
+#         self.side_b = side_b
+#
+#     def __repr__(self): # метод __repr__ повертає строкове значення об'єкта. Метод конвертує об'єкт в строку
+#         # яке тільки для внутрішнього користування
+#         return "Rectangle(%.1f, %.1f)" % (self.side_a, self.side_b)
+#
+# class Triangle:
+#
+#     def __init__(self, side):
+#         self.side = side
+#
+#     def __repr__(self):
+#         return "Triangle(%.f)" % self.side
+#
+#     @classmethod
+#     def from_rectangle(cls, rectangle):
+#         side = rectangle.side_a
+#         return cls(side)
 
 
 
@@ -213,7 +217,7 @@ class CarDealerShip:
 if __name__ == "__main__":
 
     CarDealerShip.show_cars()
-    print(Triangle.from_rectangle(20))
+
 
 """
 This is the list of cars in Dealership car_one
@@ -223,20 +227,51 @@ This is the list of cars in Dealership car_three
 
 #Приклади ІНКАПСУЛЯЦІЯ
 
+"""
+    Атрибуты, имена которых начинаются, но не заканчиваются, двумя символами
+    подчёркивания, считаются приватными. К ним применяется механизм
+    «name mangling», суть которого заключается в том, что изнутри класса
+    и его экземпляров к этим атрибутам можно обращаться по тому имени,
+     было задано при объявлении, однако на самом деле к именам слева добавляется
+    подчёркивание и имя класса. Этот механизм не предполагает защиты данных от
+    изменения извне, так как к ним всё равно можно обратиться, зная имя класса
+    и то, как Python изменяет имена приватных атрибутов, однако позволяет
+    защитить их от случайного переопределения в классах-потомках.
+"""
+
+#     class MyClass:
+#         def __init__(self):
+#             self.__private_attribute = 42
+#
+#         def get_private(self):
+#             return self.__private_attribute
+#
+# obj = MyClass()
+# print(obj.get_private())  # 42
+# print(obj.__private_attribute)  # ошибка
+# # print(obj._MyClass__private_attribute)  # 42
+
+
 
 """
  Создайте класс с именем MyObject и атрибутами с именами: простой - attribute, внутренний - inner_attribute, 
  защищенный - private_attribute и значениями соответственно 2, 3, 4. 
 Создайте объект класса c именем obj
 Выведите на печать атрибут private_attribute объекта obj
-
 """
 
 
 class MyObject:
-    def __init__(self, attribute
+    def __init__(self, attribute, _inner_attribute, __private_attribute):
+        self.attribute = 2
+        self._inner_attribute = 3
+        self.__private_attribute = 4
 
 
+obj = MyObject()
+print(obj._MyObject__private_attribute)
 
+
+#TODO розробити коректну реалізацію
 
 
