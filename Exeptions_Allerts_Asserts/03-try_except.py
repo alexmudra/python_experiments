5f75887252495a50582bc594605234c12775470d
аlexmudra <alexander.kobko@gmail.com>
2019-05-24 19:48:07 +0300

    added sample: raise & TypeError exception (compared cls arg and str)

--- Exeptions_Allerts_Asserts/03-try_except.py
+++ Exeptions_Allerts_Asserts/03-try_except.py
@@ -2,27 +2,3 @@ try:
     x = 2 / 0
 except ZeroDivisionError:
     print('Division by zero detected')
-
-"""
-Напишите код в соответствии с инструкциями в задании!
-    При a < 0, функция генерирует исключение ValueError с текстом "Invalid argument"
-"""
-def Sum(a, b):
-
-  if a < 0:
-      raise ValueError("Invalid argument")
-
-      return a + b
-print("-------------------------------------------------------------------------------")
-
-"""
-  При b = 0, функция генерирует стандартное исключение с текстом - "Division by zero"
-"""
-
-def div(a, b):
-
-    if b==0:
-
-        raise ZeroDivisionError("Division by zero")
-    return a / b
-
--- Exeptions_Allerts_Asserts/my_file_try_exept.py
+++ Exeptions_Allerts_Asserts/my_file_try_exept.py
@@ -3,9 +3,50 @@
 """
 def Sum(a, b):
 
-   try:
-       a < 0:
-   except ValueError:
-       print("Invalid argument")
+    if a < 0:
 
-       return a + b
+        raise ValueError ("Invalid argument")
+
+    return a + b
+
+
+"""
+Напишите код в соответствии с инструкциями в задании!
+    При a < 0, функция генерирует исключение ValueError с текстом "Invalid argument"
+"""
+def Sum(a, b):
+
+  if a < 0:
+      raise ValueError("Invalid argument")
+
+      return a + b
+print("-------------------------------------------------------------------------------")
+
+"""
+  При b = 0, функция генерирует стандартное исключение с текстом - "Division by zero"
+"""
+
+def div(a, b):
+
+    if b==0:
+
+        raise ZeroDivisionError("Division by zero")
+    return a / b
+
+print("-------------------------------------------------------------------------------")
+
+"""
+ При type(arg) == type('str'), функция генерирует стандартное исключение с текстом "unsupported operand type"
+"""
+def Exp(arg):
+
+    if type(arg) == type('str'):
+        raise TypeError ("unsupported operand type")
+    return arg ** 2
+
+print(Exp("skdjfkjsd"))
+
+"""
+    raise TypeError ("unsupported operand type")
+TypeError: unsupported operand type
+"""
