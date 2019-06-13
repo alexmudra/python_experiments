43077ba988650d55d5281b33c2e4b8a33ae1e3e4
mudra <alexander.kobko@gmail.com>
2019-06-13 20:50:42 +0300

    cover the list + use the format

---
+++ Iterators_generators/my_file_to_iterators_generators.py
@@ -0,0 +1,15 @@
+
+values = [1, 2, 3, 4, 5]
+
+#обходим список и используем формат
+for elem in values:
+    print('Check the value {}'.format(elem))
+
+"""
+Check the value 1
+Check the value 2
+Check the value 3
+Check the value 4
+Check the value 5
+"""
+
