eae13558de929d73f78c9047aaa57770eaf415cf
mudra <alexander.kobko@gmail.com>
2019-12-05 10:53:24 +0200

    homework_3 (slises, continue, break ect)

--- work_with_misha/hmwrk_2.py
+++ work_with_misha/hmwrk_2.py
@@ -36,13 +36,60 @@
 Программа должна вывести одно из чисел: 3 (если все совпадают), 2 (если два совпадает) или 0 (если все числа различны).
 """
 
-f_number = int(input("Input first number: "))
-s_number = int(input("Input second number: "))
-th_number = int(input("Input third number: "))
 
-if f_number == s_number == th_number:
-    print("The number are equal and value is", f_number)
-elif f_number == s_number or s_number == th_number or f_number == th_number:
-    print("2 equal numbers are available")
-elif f_number != s_number or s_number != th_number or f_number != th_number:
-    print("The numbers aren't equal")
+# f_number = int(input("Input first number: "))
+# s_number = int(input("Input second number: "))
+# th_number = int(input("Input third number: "))
+#
+# if f_number == s_number == th_number:
+#     print("The number are equal and value is", f_number)
+# elif f_number == s_number or s_number == th_number or f_number == th_number:
+#     print("2 equal numbers are available")
+# else:
+#     print("The numbers aren't equal")
+
+
+
+#====================================================================================
+
+
+"""
+1) Дано несколько чисел. Вычислите их сумму. 
+Сначала вводите количество чисел N, затем вводится ровно N целых чисел. 
+Какое наименьшее число переменных нужно для решения этой задачи?
+"""
+
+n = int(input("Input the quantity of numbers: "))
+
+a = 0
+for i in range(int(input("Enter a number: "))):
+    a += int(input("Add the number to iterator: "))
+    print(i)
+
+#не розумію
+
+
+
+#====================================================================================
+
+"""
+2) Для настольной игры используются карточки с номерами от 1 до N. 
+Одна карточка потерялась. Найдите ее, зная номера оставшихся карточек.
+Дано число N, далее N − 1 номер оставшихся карточек (различные числа от 1 до N). 
+Программа должна вывести номер потерянной карточки.
+
+Для самых умных: массивами и аналогичными структурами данных пользоваться нельзя.
+"""
+
+n = int(input())
+sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+
+for i in range(1, n):
+    sum -= int(input())
+
+print(sum)
+
+
---
+++ work_with_misha/lesson_3.py
@@ -0,0 +1,29 @@
+a = [1,2,3,5]
+
+for i in a:
+    print(i, sep="", end=", ")
+
+#    print(i, sep="", end=", " - розібратися
+
+
+"""
+=================== брейк і контінью
+"""
+
+for i in range(10):
+    if i == 5:
+        break
+
+    print(i)
+
+
+s = "lsdkflkmsdf"
+print("kdsjf", s.count(""))
+
+a, b = 1, 6
+print("before", a, b)
+print("GOOOO", a = 10, b = 12)
+
+
+a = [0 for i in range(5)] #---> генератори
+print("dsfkdsjfksdf", a)
