59549e4d87af48d3b7ee12941afa315070f920ff
mudra <alexander.kobko@gmail.com>
2020-07-10 19:19:28 +0300

    added print_info for ITVDN lesson

--- Classes_in_Python/itvdn_tests.py
+++ Classes_in_Python/itvdn_tests.py
@@ -16,3 +16,24 @@ obj2.str_field = "string2"
 
 print(obj1.int_field)
 print(obj2.str_field)
+
+
+# Опишите класс с именем Person, функция print_info которого выводит на консоль атрибуты name и age.
+# Создайте экземпляр класса – john, присвойте атрибуту name значение “John”,
+# а атрибуту age значение 22. Запустите функцию print_info() экземпляра john.
+# Запустите функцию print_info() класса Person с параметром john.
+
+class Person:
+    def print_info:
+        print(name, age)
+
+john = Person ()
+name = "John"
+age = 22
+
+print_info(john.name)
+print_info(Person.john)
+
+
+
+
